a4c62e72e8683cb36b6067addc7268a6f1501df3
mbs75 <mebuendia@alu.fpsanantonio.com>
2026-03-16 11:40:29 +0100

    Update employee names to include last names

--- SistemaGestionEmpleados.java
+++ SistemaGestionEmpleados.java
@@ -4,9 +4,9 @@ public class SistemaGestionEmpleados {
 
     public static void main(String[] args) {
         Empleados empleados = new Empleados(Constantes.CAPACIDAD_INICIAL_EMPLEADOS);
-        empleados.darAlta(new Empleado("Juan", Cargo.DESARROLLADOR, 50000));
-        empleados.darAlta(new Empleado("María", Cargo.DISEÑADORA, 45000));
-        empleados.darAlta(new Empleado("Pedro", Cargo.GERENTE, 60000));
+        empleados.darAlta(new Empleado("Juan Juan", Cargo.DESARROLLADOR, 50000));
+        empleados.darAlta(new Empleado("María María", Cargo.DISEÑADORA, 45000));
+        empleados.darAlta(new Empleado("Pedro Pedro", Cargo.GERENTE, 60000));
 
         Scanner scanner = new Scanner(System.in);
         System.out.print(Constantes.PREGUNTA_PORCENTAJE_AUMENTO);
